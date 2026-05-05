f4bb437ff36cfefd7c6a465120d083784fef986b
Kevin Rousselet <krous74@gmail.com>
2026-05-05 13:31:48 +0800

    Router fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Projects from '../views/Projects.vue'
+import About from '../views/About.vue'
+import Resume from '../views/Resume.vue'
+import Contact from '../views/Contact.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue'),
+    component: Home,
   },
   {
     path: '/projects',
     name: 'Projects',
-    component: () => import('../views/Projects.vue'),
+    component: Projects,
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue'),
+    component: About,
   },
   {
     path: '/resume',
     name: 'Resume',
-    component: () => import('../views/Resume.vue'),
+    component: Resume,
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('../views/Contact.vue'),
+    component: Contact,
+  },
+  // Catch 404 -> redirect to home
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
   },
 ]
 
 const router = createRouter({
+  // STABLE HISTORY MODE (NO # IN URL)
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
